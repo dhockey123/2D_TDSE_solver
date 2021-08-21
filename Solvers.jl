70a507f4c6ec713c2816fb0d8a33600be50b3546
dhockey123 <81137805+dhockey123@users.noreply.github.com>
2021-08-21 23:21:03 +0100

    Update Solvers.jl

--- Solvers.jl
+++ Solvers.jl
@@ -11,6 +11,9 @@ Threads.nthreads()
         A::Array{ComplexF64,2} = diff_matrix(1+2im/λ, -im/λ, Nx, Ny)
         B::Array{ComplexF64,2} = diff_matrix(1-2im/λ, im/λ, Nx, Ny)
         C::Array{ComplexF64,2} = B / A
+        V::Array{Float64,2} = par.potential_field
+        U_a::Array{ComplexF64,2}  = exp.(im*par.dt*V/2)
+        U_b::Array{ComplexF64,2}  = exp.(-im*par.dt*V/2)
     end
 
     function diff_matrix(diag,offdiag, Nx, Ny)
@@ -20,16 +23,17 @@ Threads.nthreads()
         return A
     end
 
-    function ADI_solver(ψ, par::Solver_Params)
+    function ADI_solver(ψ, Solv_par::Solver_Params)
         #Threads.@threads for i in 2:par.Nx-1
-        for i in 2:par.Nx-1
+        for i in 2:Solv_par.Nx-1
             tmp_ψ = ψ[ : , i ]
-            ψ[ : , i ] = par.C * tmp_ψ
+            ψ[ : , i ] = Solv_par.C * tmp_ψ
         end
         #Threads.@threads for j in 2:par.Ny-1
-        for j in 2:par.Ny-1
+        for j in 2:Solv_par.Ny-1
             tmp_ψ = ψ[ j , : ]
-            ψ[ j , : ] = par.C * tmp_ψ
+            ψ[ j , : ] = Solv_par.C * tmp_ψ
+            ψ[ j , : ] = (ψ[ j , : ]) .* (Solv_par.U_b[ j , : ] ./ Solv_par.U_a[ j , : ])
         end
         return ψ
     end
