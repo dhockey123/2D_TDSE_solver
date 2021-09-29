fd4eecdeed3646e3745dc8e78cf3f4691c472dc6
dhockey123 <81137805+dhockey123@users.noreply.github.com>
2021-09-29 13:22:04 +0100

    Update Solvers_2D.jl

--- Solvers_2D.jl
+++ Solvers_2D.jl
@@ -34,17 +34,18 @@ Threads.nthreads()
     function ADI_solver(ψ, Solv_par::Solver_Params)
         #Threads.@threads for i in 2:par.Nx-1
         for i in 2:Solv_par.Nx-1
-            tmp_ψ = ψ[ : , i ]
-            ψ[ : , i ] = Solv_par.C * tmp_ψ
+            #tmp_ψ = ψ[ : , i ]
+            ψ[ : , i ] = Solv_par.C * ψ[ : , i ]
         end
         #Threads.@threads for j in 2:par.Ny-1
         for j in 2:Solv_par.Ny-1
-            tmp_ψ = ψ[ j , : ]
-            ψ[ j , : ] = Solv_par.C * tmp_ψ
+            #tmp_ψ = ψ[ j , : ]
+            ψ[ j , : ] = Solv_par.C * ψ[ j , : ]
             ψ[ j , : ] = (ψ[ j , : ]) .* (Solv_par.U_b[ j , : ] ./ Solv_par.U_a[ j , : ])
         end
         return ψ
     end
+
 
     function Pseudospec_FFT(ψ, Solv_par::Solver_Params)
         
@@ -75,5 +76,51 @@ Threads.nthreads()
         # return ifft(ψ)
 
     end
+
+    # function boundary(diag, offdiag)
+    #     return [diag; offdiag]
+    # end
+
+    # # Attempt at one-way boundaries along single axis
+
+    # function ADI_solver(ψ, par, Solv_par::Solver_Params)
+    # #function ADI_solver(ψ, par, Solv_par)
+    #     tmp = zeros(ComplexF64, 1, 100)
+    #     k = par.kx_0
+
+    #     A = (-3im*k^2)/(4*par.dx) + 1/(par.dx*par.dt) + im/(2*par.dx^3) - (k^3)/(8) -3im*k/(2*par.dx) + 3*k/2
+    #     B =  (3im*k^2)/(4*par.dx) - 1/(par.dx*par.dt) - im/(2*par.dx^3) - (k^3)/(8) -3im*k/(2*par.dx) + 3*k/2
+    #     C = (3im*k^2)/4*par.dx + 1/(par.dt*par.dx) +  (k^3)/8 - (3im*k)/(2*par.dt)
+    #     D = (-3im*k^2)/(4*par.dx) - 1/(par.dt*par.dx) + (k^3)/8 - 3im*k/(2*par.dt)
+    #     E = im/(2*par.dx^3) + 3k/4
+    #     F = im/(2*par.dx^3) + 3k/4
+    #     G = -im/(2*par.dx^3) + 3k/4
+    #     H = -im/(2*par.dx^3) + 3k/4
+    
+    #     left = boundary(B, A)
+    #     tmp1 = boundary(D, C)
+    #     tmp2 = boundary(G, E)
+    #     tmp3 = boundary(H, F)
+
+    #     # for i in 1:Solv_par.Nx-1
+    #     #     tmp[i:i+1] .= (((tmp1./left) .* ψ[2 , i:i+1]) + ((tmp2./left) .* ψ[3 , i:i+1]) + ((tmp3./left) .* ψ[1 , i:i+1]  ))
+    #     # end
+    #     # (tmp1./left) .* ψ[2 , 1:1+1]
+    #     #Threads.@threads for i in 2:par.Nx-1
+    #     for i in 2:Solv_par.Nx-1
+    #         #tmp_ψ = ψ[ : , i ]
+    #         ψ[ : , i ] = Solv_par.C * ψ[ : , i ]
+    #     end
+    #     #Threads.@threads for j in 2:par.Ny-1
+    #     for j in 2:Solv_par.Ny-1
+    #         #tmp_ψ = ψ[ j , : ]
+    #         ψ[ j , : ] = Solv_par.C * ψ[ j , : ]
+    #         ψ[ j , : ] = (ψ[ j , : ]) .* (Solv_par.U_b[ j , : ] ./ Solv_par.U_a[ j , : ])
+    #     end
+        
+
+    #     return ψ
+    # end
+
 
 end
